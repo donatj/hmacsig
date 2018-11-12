09e4042a2fb197ee60ca17a370c49daefa1c9baf
Jesse Donat <donatj@gmail.com>
2018-11-12 10:21:53 -0600

    Adds example

---
+++ example/example.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/donatj/hmacsig"
+)
+
+func main() {
+	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("success"))
+	})
+
+	s := hmacsig.Handler(h, "supersecret", hmacsig.Options{})
+
+	err := http.ListenAndServe(":8080", s)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
